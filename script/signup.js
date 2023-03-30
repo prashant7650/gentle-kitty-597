1754b2ddf99e0184f69c48510cd7cfe96b159789
prashant7650 <prashantkad240999@gmail.com>
2023-03-30 22:08:19 +0530

    complte

---
+++ script/signup.js
@@ -0,0 +1,32 @@
+var data = JSON.parse(localStorage.getItem("loginobj")) || [];
+var btn = document.getElementById("signup_button");
+
+var name = document.getElementById("name");
+var mob = document.getElementById("mob");
+var email = document.getElementById("email");
+var password = document.getElementById("pass");
+console.log(name.value);
+btn.addEventListener("click", () => {
+    // console.log(name.value);
+    if (name.value == "" || mob.value == "" || email.value == "" || password.value == "") {
+        alert("Please fill in the empty fields");
+    }
+
+    else {
+        let obj = {
+            name: name.value,
+            mob: mob.value,
+            mail: email.value,
+            password: password.value
+        };
+
+        data.push(obj);
+        localStorage.setItem("loginobj", JSON.stringify(data));
+        alert("signed up successfully");
+        window.location.href="./login.html"
+        
+    }
+});
+
+
+
